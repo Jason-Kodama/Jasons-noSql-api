5a9deec893e449204938dc93855acacf36262031
Jason <jasonkodama@gmail.com>
2023-06-25 23:32:24 -0600

    added models

---
+++ models/Thoughts.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reactions');
+
+const thoughtsSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+const Thoughts = model('thoughts', thoughtsSchema);
+
+module.exports = Thoughts;
